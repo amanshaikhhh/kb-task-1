91ebefc44844c44482353c61229eb6e7d61ce6ea
amanshaikhhh <153551945+amanshaikhhh@users.noreply.github.com>
2025-10-25 01:31:21 +0530

    Add files via upload

---
+++ task-1.py
@@ -0,0 +1,32 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+print("Select operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+choice = input("Enter choice (1/2/3/4): ")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == '1':
+    print(num1, "+", num2, "=", add(num1, num2))
+elif choice == '2':
+    print(num1, "-", num2, "=", sub(num1, num2))
+elif choice == '3':
+    print(num1, "*", num2, "=", mul(num1, num2))
+elif choice == '4':
+    print(num1, "/", num2, "=", div(num1, num2))
+else:
+    print("Invalid Input")
